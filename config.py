3d3d0206e36a3c0e2f2144e534c8bf4f4bf115ac
ali ahnach <ahnachali25@gmail.com>
2026-07-19 19:34:44 +0100

    changement dans back

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
+    SECRET_KEY = "change-this-secret-key"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://rdv_user:motdepasse@localhost/rdv_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
