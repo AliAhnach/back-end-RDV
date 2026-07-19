59f2e5c3d29093724a2a6f0351e280d94a510ec6
ali ahnach <ahnachali25@gmail.com>
2026-07-19 19:40:51 +0100

    corroger accers vers base de donnees

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+import os
+
 class Config:
-    SECRET_KEY = "change-this-secret-key"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://rdv_user:motdepasse@localhost/rdv_db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key")
+    _db_url = os.environ.get("DATABASE_URL", "mysql+pymysql://rdv_user:motdepasse@localhost/rdv_db")
+    SQLALCHEMY_DATABASE_URI = _db_url.replace("mysql://", "mysql+pymysql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
